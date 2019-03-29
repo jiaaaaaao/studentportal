75676f435aa1f8960b31638b3c24d7274f6d74ca
Jiao Wang <jiaowang@Jiaos-MacBook-Pro.local>
2019-03-28 22:06:20 -0700

    init

--- portal/src/main/java/student/portal/StudentInfo.java
+++ portal/src/main/java/student/portal/StudentInfo.java
@@ -1,11 +1,14 @@
 package student.portal;
 
-public class StudentInfo {
-    public  long id;
-    public String name;
-    double chineseScore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-    public StudentInfo(long id, String name, double chineseScore) {
+public class StudentInfo {
+    public long id;
+    public String name;
+    public double chineseScore;
+
+    public StudentInfo(@JsonProperty("id") long id, @JsonProperty("name") String name,
+	    @JsonProperty("chineseScore") double chineseScore) {
 	this.id = id;
 	this.name = name;
 	this.chineseScore = chineseScore;
@@ -21,6 +24,18 @@ public class StudentInfo {
 
     public double getChineseScore() {
 	return chineseScore;
+    }
+
+    public void setId(long id) {
+	this.id = id;
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public void setChineseScore(double chineseScore) {
+	this.chineseScore = chineseScore;
     }
 
 }
